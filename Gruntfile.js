1293f9ecdcf0f8453bb6702399a7ca5fca0baf09
Ryan Taylor Long <ryan@rtlong.com>
2015-03-19 22:27:24 -0700

    Add Gruntfile

---
+++ Gruntfile.js
@@ -0,0 +1,32 @@
+module.exports = function (grunt) {
+  packageJSON = grunt.file.readJSON('package.json')
+  grunt.initConfig({
+    pkg: packageJSON,
+    jshint: {
+      files: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js'],
+      options: packageJSON.jshintConfig,
+    },
+    jscs: {
+      files: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js'],
+      options: {
+        config: '.jscsrc',
+      },
+    },
+    watch: {
+      files: ['<%= jshint.files %>'],
+      tasks: ['jshint', 'jscs', 'shell:test'],
+    },
+    shell: {
+      test: {
+        command: 'npm test'
+      }
+    }
+  })
+
+  grunt.loadNpmTasks('grunt-shell');
+  grunt.loadNpmTasks('grunt-contrib-jshint')
+  grunt.loadNpmTasks('grunt-contrib-watch')
+  grunt.loadNpmTasks('grunt-jscs')
+
+  grunt.registerTask('default', ['jshint', 'jscs', 'shell:test'])
+}
