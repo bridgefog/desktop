eee51e3e575bdde55df6e7f8ce32dbfe55f7adca
Nathan Amick <github@nathanamick.com>
2015-03-20 01:36:14 -0700

    Add badge

--- test/badge.js
+++ test/badge.js
@@ -2,19 +2,21 @@
 
 var assert = require('assert')
 var vows = require('vows')
-var ipfs = require('../lib/ipfs-api-client')('localhost', 9999)
-var badge = require('../lib/badge')
+  // var ipfs = require('../lib/ipfs-api-client')('localhost', 9999)
+var Badge = require('../lib/badge')
 
 vows.describe('Badge').addBatch({
 
-  currentName: {
-    topic: function () {
-      var now = Date.now()
-      new badge.currentName(now)
-    },
-    'returns the current string to be hashed into a badge': function (name) {
-      assert.equal(name, "AllTheMusic:" + now * 0.001 * 0.001)
+  currentName: (function () {
+    var now = Date.now()
+    return {
+      topic: function () {
+        return new Badge().currentName(now)
+      },
+      'returns the current string to be hashed into a badge': function (name) {
+        assert.equal(name, 'AllTheMusic:' + Math.round(now / (1000 * 60 * 60)))
+      }
     }
-  }
+  })()
 
-})
+}).export(module)
