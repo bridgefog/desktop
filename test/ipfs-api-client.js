19f395bfd551f0114e0fbb5509eb035cb8fdb83a
Nathan Amick <github@nathanamick.com>
2015-03-24 00:09:39 -0700

    Add peerID

--- test/ipfs-api-client.js
+++ test/ipfs-api-client.js
@@ -7,11 +7,26 @@ var ipfs_endpoint = url.parse('http://localhost:9999/api/v0')
 var ipfs = require('../lib/ipfs-api-client')(ipfs_endpoint)
 var DagObject = require('../lib/dag-object')
 
+var osenv = require('osenv')
+var fs = require('fs')
+var ipfsConfig = JSON.parse(fs.readFileSync(osenv.home() + '/.go-ipfs/config'))
+
 var knownHashes = {
   foo: 'QmWqEeZS1HELySbm8t8U55UkBe75kaLj9WnFb882Tkf5NL'
 }
 
 describe('IPFS API', function () {
+  describe('peerID', function () {
+    it('returns the peerID of this local node', function (done) {
+      ipfs.peerID(function (error, result) {
+        if (error) { return done(error) }
+
+        assert.equal(result, ipfsConfig.Identity.PeerID)
+        done()
+      })
+    })
+  })
+
   describe('addObject', function () {
     it('returns a thing with the correct Hash', function (done) {
       var dagNode = new DagObject({
