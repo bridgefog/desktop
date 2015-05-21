5aac39f90d39693d881c997e5a8674d590d1171f
Ryan Taylor Long <ryan@rtlong.com>
2015-05-20 20:47:55 -0700

    Fix bug in bin/demo-alice.js

--- bin/demo-alice.js
+++ bin/demo-alice.js
@@ -5,7 +5,7 @@
 import R from 'ramda'
 import demo from '../demos/alice'
 
-if (R.intersection(['ipfs', 'alice'], process.env.NODE_DEBUG.split(',')).length === 0) {
+if (R.intersection(['ipfs', 'alice'], (process.env.NODE_DEBUG || '').split(',')).length === 0) {
   console.log('Set NODE_DEBUG to include "ipfs" or "alice" to see more log output')
 }
 
