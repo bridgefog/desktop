4aa624a87545b7f04a37cd06516766367fefcf8f
Ryan Taylor Long <ryan@rtlong.com>
2015-06-04 01:46:01 -0700

    Add a wear-badge executable

---
+++ bin/wear-badge.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env babel-node
+
+'use strict'
+
+import { IPFSClient, util as ipfsUtil } from 'atm-ipfs-api'
+import Clubnet from '../lib/clubnet'
+import Badge from '../lib/badge'
+
+var ipfs = new IPFSClient(ipfsUtil.ipfsEndpoint())
+var clubnet = new Clubnet(ipfs, () => new Badge())
+
+clubnet.wearBadge()
+  .then(key => console.log(key))
