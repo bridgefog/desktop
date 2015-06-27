e89d350525e2d9b65935d1da540b0c30fdbc03d9
Ryan Taylor Long <ryan@rtlong.com>
2015-06-27 13:38:09 -0700

    Improve build process and have it create a dmg

--- build.sh
+++ build.sh
@@ -4,17 +4,24 @@ set -x -e
 
 ./node_modules/.bin/gulp dist
 
-rm -rf pkg/Beam.app
+rm -rf pkg/app/Beam.app
 
-cp -a node_modules/electron-prebuilt/dist/Electron.app pkg/Beam.app
+mkdir -p pkg/app
+cp -a node_modules/electron-prebuilt/dist/Electron.app pkg/app/Beam.app
 
-mkdir -pv ./pkg/Beam.app/Contents/Resources/app
-cp -v pkg/Info.plist pkg/Beam.app/Contents/Info.plist
+mkdir -pv ./pkg/app/Beam.app/Contents/Resources/app
+cp -v pkg/Info.plist pkg/app/Beam.app/Contents/Info.plist
 
-rsync -aP --delete --include '/package.json' --include '/dist/***' --exclude '*' ./ ./pkg/Beam.app/Contents/Resources/app/
+rsync -aP --delete --include '/package.json' --include '/dist/***' --exclude '*' ./ ./pkg/app/Beam.app/Contents/Resources/app/
 
 (
-  cd ./pkg/Beam.app/Contents/Resources/app/;
+  cd ./pkg/app/Beam.app/Contents/Resources/app/;
   export NODE_ENV=production
   npm install
 )
+
+# Create the .dmg image
+# size=$(du -sm pkg/app | awk '{ print $1 }')
+# hdiutil create -size ${size} -fs HFS+ -volname "My Volume" myimg.dmg
+rm -f pkg/beam.dmg
+hdiutil create -srcfolder pkg/app -volname beam pkg/beam.dmg
