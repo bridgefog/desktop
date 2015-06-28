8029f15bab89952c2fb7b664662f85976716006f
Ryan Taylor Long <ryan@rtlong.com>
2015-06-27 19:16:34 -0700

    Improve build script and use BridgeFog name instead of beam

--- build.sh
+++ build.sh
@@ -2,26 +2,60 @@
 
 set -x -e
 
-./node_modules/.bin/gulp dist
+prepare_dist() {
+  ./node_modules/.bin/gulp dist
+}
 
-rm -rf pkg/app/Beam.app
+get_ipfs() {
+  version=0.3.5
+  filename="ipfs_v${version}_darwin-amd64.zip"
+  url="https://gobuilder.me/get/github.com/ipfs/go-ipfs/cmd/ipfs/${filename}"
 
-mkdir -p pkg/app
-cp -a node_modules/electron-prebuilt/dist/Electron.app pkg/app/Beam.app
+  mkdir -p tmp
+  pushd tmp >&2
 
-mkdir -pv ./pkg/app/Beam.app/Contents/Resources/app
-cp -v pkg/Info.plist pkg/app/Beam.app/Contents/Info.plist
+  test -f "${filename}" || curl -sSLO "${url}" >&2
+  unzip -o "${filename}" >&2
+  ipfs_path="$PWD/ipfs/ipfs"
+  popd >&2
+  if [ ! -x "${ipfs_path}" ]; then
+    echo "IPFS was not obtained successfully"
+    exit 1
+  fi
+  echo "${ipfs_path}"
+}
 
-rsync -aP --delete --include '/package.json' --include '/dist/***' --exclude '*' ./ ./pkg/app/Beam.app/Contents/Resources/app/
+copy_dot_app() {
+  rm -rf pkg/app/BridgeFog.app
+  mkdir -p pkg/app
+  cp -a node_modules/electron-prebuilt/dist/Electron.app pkg/app/BridgeFog.app
+  cp pkg/Info.plist pkg/app/BridgeFog.app/Contents/Info.plist
+}
 
-(
-  cd ./pkg/app/Beam.app/Contents/Resources/app/;
+copy_dist_into_dot_app() {
+  mkdir -pv pkg/app/BridgeFog.app/Contents/Resources/app
+  rsync -aP --delete --include '/package.json' --include '/dist/***' --exclude '*' ./ ./pkg/app/BridgeFog.app/Contents/Resources/app/
+
+  pushd ./pkg/app/BridgeFog.app/Contents/Resources/app/;
   export NODE_ENV=production
   npm install
-)
+  popd
+}
 
-# Create the .dmg image
-# size=$(du -sm pkg/app | awk '{ print $1 }')
-# hdiutil create -size ${size} -fs HFS+ -volname "My Volume" myimg.dmg
-rm -f pkg/beam.dmg
-hdiutil create -srcfolder pkg/app -volname beam pkg/beam.dmg
+copy_ipfs_into_dot_app() {
+  ipfs_path="$1"
+  mkdir -p ./pkg/app/BridgeFog.app/Contents/Resources/app/vendor;
+  cp -a "$ipfs_path" ./pkg/app/BridgeFog.app/Contents/Resources/app/vendor;
+}
+
+package_dmg() {
+  rm -f pkg/bridgefog.dmg
+  hdiutil create -srcfolder pkg/app -volname BridgeFog pkg/BridgeFog.dmg
+}
+
+prepare_dist
+ipfs_path=$(get_ipfs)
+copy_dot_app
+copy_dist_into_dot_app
+copy_ipfs_into_dot_app
+package_dmg
