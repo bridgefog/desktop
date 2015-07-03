b3c834cf04283a3a87e352b596a722d8e3aba209
Nathan Amick <github@nathanamick.com>
2015-07-02 18:12:05 -0700

    Support jpeg and png images

--- bin/add-mp3-local-coverart.js
+++ bin/add-mp3-local-coverart.js
@@ -4,11 +4,18 @@
 
 import MusicCollection from '../lib/upload/music-collection'
 import Contents from '../lib/contents'
+import glob from 'glob'
 
-var collection = new MusicCollection(process.argv.slice(2))
+var filenames = glob.sync(process.argv[2] + '/*')
+var collection = new MusicCollection(filenames)
 
 if (collection.musicFiles.length === 0) {
-  console.error('ERROR: Supply one or more *.mp3 filename arguments')
+  console.error('ERROR: No music found.')
+  process.exit(1)
+}
+
+if (collection.imageFiles.length === 0) {
+  console.error('ERROR: No cover art found')
   process.exit(1)
 }
 
