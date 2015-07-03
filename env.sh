9b1f4672d3e44e0805f9820ca2c0568f54653fc6
Ryan Taylor Long <ryan@rtlong.com>
2015-07-03 16:11:24 -0700

    Add ipfs-select function to env.sh
    This makes it simple to switch between the nested IPFS db or your global user
    one

--- env.sh
+++ env.sh
@@ -6,3 +6,13 @@
 # NODE_ENV=development
 
 export PATH=$PWD/node_modules/.bin:$PATH
+
+ipfs-select() {
+  if [[ $1 == "app" ]]; then
+    export IPFS_PATH="$HOME/Library/Application Support/com.bridgefog.beam/ipfs"
+    echo 'ipfs-select: Using app-embedded IPFS_PATH' 1>&2
+  else
+    unset IPFS_PATH
+    echo 'ipfs-select: Using default IPFS_PATH' 1>&2
+  fi
+}
