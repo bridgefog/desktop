204f40f1fc9c56fa77448eb4a0c1a1db9d177731
Ryan Taylor Long <ryan@rtlong.com>
2015-07-08 22:47:44 -0700

    Add ipfs_endpoint to env.sh

--- env.sh
+++ env.sh
@@ -10,9 +10,11 @@ export PATH=$PWD/node_modules/.bin:$PATH
 ipfs-select() {
   if [[ $1 == "app" ]]; then
     export IPFS_PATH="$HOME/Library/Application Support/com.bridgefog.beam/ipfs"
+    export ipfs_endpoint='http://localhost:4003'
     echo 'ipfs-select: Using app-embedded IPFS_PATH' 1>&2
   else
     unset IPFS_PATH
+    export ipfs_endpoint='http://localhost:5001'
     echo 'ipfs-select: Using default IPFS_PATH' 1>&2
   fi
 }
