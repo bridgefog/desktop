159095e30ca8ed621fbe2c6d1e57fcc3bcf5f9d7
Ryan Taylor Long <ryan@rtlong.com>
2015-07-13 22:00:11 -0700

    make discovery/swarm capable of outputting new-resolved pubtree keys

---
+++ test/unit/discovery/swarm.js
@@ -0,0 +1,24 @@
+import { assert } from 'chai'
+import Swarm from '../../../lib/discovery/swarm'
+
+describe('Discovery/Swarm', () => {
+  describe('constructor', () => {
+    it('accepts this.onKey in object argument', () => {
+      var cb = Function()
+      var swarm = new Swarm({ onKey: cb })
+      assert.equal(swarm.onKey, cb)
+    })
+  })
+
+  describe('#gotKey()', () => {
+    it('calls this.onKey with the key', (done) => {
+      var calledwith
+      var swarm = new Swarm({})
+      swarm.onKey = key => {
+        assert.equal(key, 'QmYNmQKp6SuaVrpgWRsPTgCQCnpxUYGq76YEKBXuj2N4H6')
+        done()
+      }
+      swarm.gotKey('QmYNmQKp6SuaVrpgWRsPTgCQCnpxUYGq76YEKBXuj2N4H6')
+    })
+  })
+})
