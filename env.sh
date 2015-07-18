b235529bf83cb8a20a4df46099be17b0259eccfd
Ryan Taylor Long <ryan@rtlong.com>
2015-07-17 20:02:09 -0700

    Use levelDB with jsondown to store release info

--- env.sh
+++ env.sh
@@ -9,7 +9,7 @@ export PATH=$PWD/node_modules/.bin:$PATH
 
 ipfs-select() {
   if [[ $1 == "app" ]]; then
-    export IPFS_PATH="$HOME/Library/Application Support/com.bridgefog.beam/ipfs"
+    export IPFS_PATH="$HOME/Library/Application Support/com.bridgefog.fog/ipfs"
     export ipfs_endpoint='http://localhost:4003'
     echo 'ipfs-select: Using app-embedded IPFS_PATH' 1>&2
   else
